7bf2660d6d411cba74dccd1a2ef5a6d44e9093bd
quangmta <nguyennhuquang1998@gmail.com>
2023-10-14 16:45:19 +0300

    first

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def gernerate_launch_description():
+def generate_launch_description():
     return LaunchDescription([
         Node(
             package='demo_nodes_py',
